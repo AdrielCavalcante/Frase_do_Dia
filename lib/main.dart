395fba7a24ae8901b77ada26169664e30760897a
AdrielCavalcante <adriel.cavalcante2004@gmail.com>
2023-07-15 22:21:28 -0300

    :sparkles: feat: Interface melhorada

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,27 @@
-import 'dart:ffi';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Frases do dia',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Frases do dia'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomeState extends State<Home> {
   int _randomNumber = 0;
   int _random = -1;
   bool _repeat = false;
-  List<String> _phrases = [
+
+  final List<String> _phrases = [
     "Algumas frases não devem ser verdade...",
     "Nunca jogue de Yummy nas sextas",
     "Talvez esse app seja muito pesado...",
@@ -45,15 +29,18 @@ class _MyHomePageState extends State<MyHomePage> {
     "Você terá um ótimo dia"
   ];
 
-  void _randomPhrases() {
+  String _phrase = "Clique abaixo para gerar uma frase:";
+
+  void _generatePhrases() {
     setState(() {
       // para não termos repetições de frases
       do {
-        _randomNumber = new Random().nextInt(5);
+        _randomNumber = new Random().nextInt(_phrases.length);
         if(_randomNumber == _random) {
           _repeat = true;
         } else {
           _repeat = false;
+          _phrase = _phrases[_randomNumber];
         }
         _random = _randomNumber;
       } while(_repeat);
@@ -64,28 +51,50 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Frase do dia"),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: Center(
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Pressione o botão para gerar uma frase:\n',
+            SvgPicture.asset(
+              'images/icon.svg',
+              height: 250,
             ),
             Text(
-              _phrases[_randomNumber],
-              style: Theme.of(context).textTheme.labelLarge,
+              _phrase,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                color: Colors.black
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurpleAccent, // Define a cor de fundo do botão
+                  padding: EdgeInsets.all(16), // Define o padding do botão
+                ),
+                child: Text(
+                  "Nova frase",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                onPressed: _generatePhrases,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _randomPhrases,
-        tooltip: 'add a phrase',
-        child: const Icon(Icons.pending_outlined),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
